530347000f1c5b69a02dffa94c7352b4fd7dbb3d
sggranil <dev.simongranil@gmail.com>
2025-01-21 06:23:13 +0800

    :hammer: add mock data

--- consumer/views.py
+++ consumer/views.py
@@ -1,7 +1,26 @@
 import django.template.response
+import cluster_iq.utils
 
 
 def index(request):
     template_path = 'pages/consumer.page.html'
+    shopping_preferences = cluster_iq.utils.chart_count_formatter(
+        ['In-store', 'Online'],
+        [60, 40]
+    )
+    product_preferences = cluster_iq.utils.chart_count_formatter(
+        ['Food and Beverages', 'Personal Care', 'Clothing', 'Electronics'],
+        [45, 30, 15, 10]
+    )
+    spending_patterns = cluster_iq.utils.chart_count_formatter(
+        ['18-24', '25-34', '35-44', '45+'],
+        [1500, 2500, 3500, 4000]
+    )
 
-    return django.template.response.TemplateResponse(request, template_path)
+    context = {
+        "shopping_preferences": shopping_preferences,
+        "product_preferences": product_preferences,
+        "spending_patterns": spending_patterns
+    }
+
+    return django.template.response.TemplateResponse(request, template_path, context=context)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,18 +1,18 @@
 import django.template.response
-import dashboard.utils
+import cluster_iq.utils
 
 
 def index(request):
     template_path = 'pages/dashboard.page.html'
-    age_group = dashboard.utils.chart_count_formatter(
+    age_group = cluster_iq.utils.chart_count_formatter(
         ['18 - 24 years old', '25 - 34 years old', '35 - 44 years old', '45 - 54 years old', '55+ years old'],
         [4, 6, 8, 10, 2]
     )
-    monthly_spending = dashboard.utils.chart_count_formatter(
+    monthly_spending = cluster_iq.utils.chart_count_formatter(
         ['₱0 - ₱999', '₱1,000 - ₱2,999', '₱3,000 - ₱4,999', '₱5,000+'],
         [9, 10, 5, 3]
     )
-    shopping_frequency = dashboard.utils.chart_count_formatter(
+    shopping_frequency = cluster_iq.utils.chart_count_formatter(
         ['Weekly', 'Monthly', 'Less Often'],
         [9, 1, 2]
     )
