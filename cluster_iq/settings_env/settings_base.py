5b6f315ad913dcc5ce7b60132b921681b10a4be4
sggranil <dev.simongranil@gmail.com>
2025-01-21 15:42:25 +0800

    :wrench: instantiate media url

--- cluster_iq/settings_env/settings_base.py
+++ cluster_iq/settings_env/settings_base.py
@@ -126,6 +126,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "../static/"),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
